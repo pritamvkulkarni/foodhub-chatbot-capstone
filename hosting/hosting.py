f9cfa9d6624ce27fab78bee27bd795c69fa78933
pritamvkulkarni <pritamvkulkarni@gmail.com>
2025-10-25 15:16:33 +0000

    Setup foodhub chatbot MLOps pipeline

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -6,7 +6,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 
 # Upload the deployment folder to your Hugging Face Space
 api.upload_folder(
-    folder_path="foodhub-chatbot/deployment",     # Local folder to upload
+    folder_path="deployment",     # Local folder to upload
     repo_id="pkulkar/foodhub-chatbot",      # Replace with your Hugging Face Space ID
     repo_type="space",                            # Type of repo: space, model, or dataset
     path_in_repo="",                              # Optional: subfolder path inside the repo
